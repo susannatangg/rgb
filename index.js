c89c1d497c2070e3a823e34bcaf15fd38c037758
Susanna <s00tangg@gmail.com>
2022-05-25 19:57:13 -0700

    changed font

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ window.onload=function(){
                     console.log("correct");
                     score.textContent=parseInt(score.textContent)+1;
                 }else{
+                    // circles[i].style.borderColor="red";
+                    // circles[i].style.borderWidth="thick";
+                    //circles[i].classList.add("wrong-border");
                     if(parseInt(score.textContent)>parseInt(highScore.textContent)){
                         highScore.textContent=score.textContent;
                     }
